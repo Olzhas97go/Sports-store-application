c1c27b811acb6fd327fe0c3043ec058de500c761
Olzhas Olzhas <Olzhas-1997@mail.ru>
2024-05-19 11:34:26 +0500

    Add styling the content.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,17 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models.Repository;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
-  public class HomeController : Controller
+    public class HomeController : Controller
     {
-        private readonly IStoreRepository _repository;
+        private readonly IStoreRepository repository;
 
         public HomeController(IStoreRepository repository)
         {
-            _repository = repository;
+            this.repository = repository;
         }
 
-        public IActionResult Index() => View(_repository.Products);
+        public int PageSize = 4;
+
+        public ViewResult Index(int productPage = 1)
+        {
+            return View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                               .OrderBy(p => p.ProductId)
+                               .Skip((productPage - 1) * PageSize)
+                               .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count(),
+                },
+            });
+        }
+
     }
 }
